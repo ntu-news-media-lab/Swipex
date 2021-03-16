492bd2809ba45fe656f2d197603d0e5c82db92c4
lihaan <45350323+lihaan@users.noreply.github.com>
2021-03-16 18:21:04 +0800

    WebArticle fadein card overlay

--- web/swipex/src/WebArticle.js
+++ web/swipex/src/WebArticle.js
@@ -1,15 +1,22 @@
-import today_screenshot from './res/homepage.png';
-import './WebArticle.css';
+import today_screenshot from "./res/homepage.png";
+import "./WebArticle.css";
 
 function WebArticle() {
   return (
     <div className="web_article">
-    <img src={today_screenshot} />
-    <button className="explore_btn" onClick={() => document.getElementsByClassName("article_overlay")[0].style.display="block"} >
-      Explore
-    </button>
+      <img src={today_screenshot} className="img_today_screenshot"/>
+      <button
+        className="explore_btn"
+        onClick={() => {
+          document.getElementsByClassName("article_overlay")[0].style.display = "block";
+          setTimeout(function () {
+            document.getElementsByClassName("article_overlay")[0].classList.add("fadein");
+          }, 10);
+        }}
+      >
+        Explore
+      </button>
     </div>
-    
   );
 }
 
