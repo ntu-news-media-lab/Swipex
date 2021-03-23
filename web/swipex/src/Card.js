df667c724ba4b88c52d88c3e445c7f67b75b5bb3
Xuefuguoalita <50324940+Xuefuguoalita@users.noreply.github.com>
2021-03-23 21:25:09 +0800

    Added card counter
    Added card counter

--- web/swipex/src/Card.js
+++ web/swipex/src/Card.js
@@ -10,11 +10,13 @@ function Card(props) {
   return (
     <animated.div className="card" style={props.style}>
       <div className="card-category">{props.category}</div>
+      <div className="card-exit"></div>
       <div className="card-content">
         <div className="card-image-container">
           <img src={props.image} className="main-img" />
           <img src={props.image} className="blur-img" />
         </div>
+        <div className="card-counter">{props.counter}</div>
         <div className="card-title">{props.title}</div>
         <div className="card-summary">{props.summary}</div>
       </div>
